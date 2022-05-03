d008627752ec4512c3d4395628a232f9bf346a02
GOCrazy <83633399+gocrazygh@users.noreply.github.com>
2022-05-03 20:53:45 +0800

    Create fizzbuzz.go

---
+++ fizzbuzz/fizzbuzz.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 0; i < 100; i++ {
+		fmt.Println(i)
+		if i % 3 == 0 {
+			fmt.Println(i, "Fizz")
+		}else if i % 5 == 0{
+			fmt.Println(i, "Buzz")
+		}
+	}
+}
